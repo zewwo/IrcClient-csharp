0e3cd500623f60749639c3f8922e39271af92833
cory_shivers <shives.cory@gmail.com>
2014-08-02 21:17:41 -0500

    added demo project for testing

---
+++ ClientDemo/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace ClientDemo
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.LabelServer = new System.Windows.Forms.Label();
+            this.LabelPort = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lstUsers = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtChannel = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNick = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(59, 32);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(100, 20);
+            this.txtServer.TabIndex = 0;
+            this.txtServer.Text = "irc.voltirc.com";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(200, 32);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(44, 20);
+            this.txtPort.TabIndex = 1;
+            this.txtPort.Text = "6667";
+            // 
+            // LabelServer
+            // 
+            this.LabelServer.AutoSize = true;
+            this.LabelServer.Location = new System.Drawing.Point(12, 34);
+            this.LabelServer.Name = "LabelServer";
+            this.LabelServer.Size = new System.Drawing.Size(41, 13);
+            this.LabelServer.TabIndex = 2;
+            this.LabelServer.Text = "Server:";
+            // 
+            // LabelPort
+            // 
+            this.LabelPort.AutoSize = true;
+            this.LabelPort.Location = new System.Drawing.Point(165, 35);
+            this.LabelPort.Name = "LabelPort";
+            this.LabelPort.Size = new System.Drawing.Size(29, 13);
+            this.LabelPort.TabIndex = 3;
+            this.LabelPort.Text = "Port:";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(401, 30);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(72, 23);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Location = new System.Drawing.Point(12, 58);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.ReadOnly = true;
+            this.rtbOutput.Size = new System.Drawing.Size(336, 288);
+            this.rtbOutput.TabIndex = 5;
+            this.rtbOutput.Text = "";
+            // 
+            // txtSend
+            // 
+            this.txtSend.Enabled = false;
+            this.txtSend.Location = new System.Drawing.Point(12, 352);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(255, 20);
+            this.txtSend.TabIndex = 6;
+            this.txtSend.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSend_KeyDown);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Enabled = false;
+            this.btnSend.Location = new System.Drawing.Point(273, 350);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 7;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // lstUsers
+            // 
+            this.lstUsers.FormattingEnabled = true;
+            this.lstUsers.IntegralHeight = false;
+            this.lstUsers.Location = new System.Drawing.Point(354, 58);
+            this.lstUsers.Name = "lstUsers";
+            this.lstUsers.Size = new System.Drawing.Size(119, 288);
+            this.lstUsers.Sorted = true;
+            this.lstUsers.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(247, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Channel:";
+            // 
+            // txtChannel
+            // 
+            this.txtChannel.Location = new System.Drawing.Point(302, 32);
+            this.txtChannel.Name = "txtChannel";
+            this.txtChannel.Size = new System.Drawing.Size(93, 20);
+            this.txtChannel.TabIndex = 10;
+            this.txtChannel.Text = "#TechLifeForum";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Nick:";
+            // 
+            // txtNick
+            // 
+            this.txtNick.Location = new System.Drawing.Point(59, 6);
+            this.txtNick.Name = "txtNick";
+            this.txtNick.Size = new System.Drawing.Size(100, 20);
+            this.txtNick.TabIndex = 12;
+            this.txtNick.Text = "Guest";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 383);
+            this.Controls.Add(this.txtNick);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtChannel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstUsers);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.rtbOutput);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.LabelPort);
+            this.Controls.Add(this.LabelServer);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtServer);
+            this.Name = "frmMain";
+            this.Text = "Client";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label LabelServer;
+        private System.Windows.Forms.Label LabelPort;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ListBox lstUsers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtChannel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNick;
+    }
+}
+
---
+++ ClientDemo/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using TechLifeForum;
+using System.Windows.Forms;
+
+namespace ClientDemo
+{
+    public partial class frmMain : Form
+    {
+        IrcClient client;
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            Random r = new Random();
+
+            for (int i = 0; i < 3; i++)
+                txtNick.AppendText(r.Next(10).ToString());
+        }
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (btnConnect.Text == "Connect")
+                DoConnect();
+            else
+                DoDisconnect();
+        }
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (client.Connected && !String.IsNullOrEmpty(txtSend.Text.Trim()))
+            {
+                if (txtChannel.Text.StartsWith("#"))
+                    client.SendMessage(txtChannel.Text.Trim(), txtSend.Text.Trim());
+                else
+                    client.SendMessage("#" + txtChannel.Text.Trim(), txtSend.Text.Trim());
+
+                AddToChatWindow("Me: " + txtSend.Text.Trim());
+                txtSend.Clear();
+                txtSend.Focus();
+            }
+        }
+        private void txtSend_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                btnSend.PerformClick();
+        }
+
+        private void DoConnect()
+        {
+            if (String.IsNullOrEmpty(txtServer.Text.Trim()))
+            {
+                MessageBox.Show("Please specify a server");
+                return;
+            }
+            if (String.IsNullOrEmpty(txtChannel.Text.Trim()))
+            {
+                MessageBox.Show("Please specify a channel");
+                return;
+            }
+            if (String.IsNullOrEmpty(txtNick.Text.Trim()))
+            {
+                MessageBox.Show("Please specify a nick");
+                return;
+            }
+
+            int port;
+            if (Int32.TryParse(txtPort.Text, out port))
+                client = new IrcClient(txtServer.Text.Trim(), port);
+            else
+                client = new IrcClient(txtServer.Text.Trim());
+
+            AddEvents();
+            client.Nick = txtNick.Text.Trim();
+
+            btnConnect.Enabled = false;
+            txtChannel.Enabled = false;
+            txtPort.Enabled = false;
+            txtServer.Enabled = false;
+            txtNick.Enabled = false;
+            rtbOutput.Clear(); // in case they reconnect and have old stuff there
+
+
+            client.Connect();
+        }
+        private void DoDisconnect()
+        {
+
+            btnConnect.Enabled = true;
+            txtChannel.Enabled = true;
+            txtPort.Enabled = true;
+            txtServer.Enabled = true;
+            txtNick.Enabled = true;
+
+            lstUsers.Items.Clear();
+            txtSend.Enabled = false;
+            btnSend.Enabled = false;
+
+            client.Disconnect();
+            client = null;
+
+            btnConnect.Text = "Connect";
+        }
+        private void AddEvents()
+        {
+            client.ChannelMessage += client_ChannelMessage;
+            client.ExceptionThrown += client_ExceptionThrown;
+            client.NoticeMessage += client_NoticeMessage;
+            client.OnConnect += client_OnConnect;
+            client.PrivateMessage += client_PrivateMessage;
+            client.ServerMessage += client_ServerMessage;
+            client.UpdateUsers += client_UpdateUsers;
+            client.UserJoined += client_UserJoined;
+            client.UserLeft += client_UserLeft;
+            client.UserNickChange += client_UserNickChange;
+        }
+        private void AddToChatWindow(string message)
+        {
+            rtbOutput.AppendText(message + "\n");
+            rtbOutput.ScrollToCaret();
+        }
+
+        #region Event Listeners
+
+        void client_OnConnect()
+        {
+
+            txtSend.Enabled = true;
+            txtSend.Focus();
+            btnSend.Enabled = true;
+
+            btnConnect.Text = "Disconnect";
+            btnConnect.Enabled = true;
+
+            if (txtChannel.Text.StartsWith("#"))
+                client.JoinChannel(txtChannel.Text.Trim());
+            else
+                client.JoinChannel("#" + txtChannel.Text.Trim());
+
+        }
+
+        void client_UserNickChange(string oldUser, string newUser)
+        {
+            lstUsers.Items[lstUsers.Items.IndexOf(oldUser)] = newUser;
+        }
+
+        void client_UserLeft(string Channel, string User)
+        {
+            lstUsers.Items.Remove(User);
+        }
+
+        void client_UserJoined(string Channel, string User)
+        {
+            lstUsers.Items.Add(User);
+        }
+
+        void client_UpdateUsers(string Channel, string[] userlist)
+        {
+            lstUsers.Items.Clear();
+            lstUsers.Items.AddRange(userlist);
+            
+        }
+
+        void client_ServerMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        void client_PrivateMessage(string User, string Message)
+        {
+            AddToChatWindow("PM FROM " + User + ": " + Message);
+        }
+
+        void client_NoticeMessage(string User, string Message)
+        {
+            AddToChatWindow("NOTICE FROM " + User + ": " + Message);
+        }
+
+        void client_ExceptionThrown(Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+
+        void client_ChannelMessage(string Channel, string User, string Message)
+        {
+            AddToChatWindow(User + ": " + Message);
+        }
+
+        #endregion
+
+    }
+}
